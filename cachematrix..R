f04af9162336d57a9809691612cea26451a2fe98
Shelly Zhu <xzhu@ysu.edu>
2019-01-06 21:48:46 -0500

    1. makeCacheMatrix（）: This function creates the inverse matrix . Then assign the inverse matrix to a global variable. 2. cacheSolve(): This function check the value of the inverse matrix in global variable. if it is not NULL, which means that the makeCacheMatrix() set the value success. If it is NULL, then I need calculate the inverse matrix  and assign it to the global variable.
    Signed-off-by: Shelly Zhu

--- cachematrix..R
+++ cachematrix..R
@@ -1,35 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+## the first funciton MakeCacheMatrix() set the inverse of the matrix to a global variable 
+## the second function cacheSolve() get the value of the global verialbe that MakeCacheMatrix() set. 
 
-## Write a short comment describing this function
+
+## Declare a empty global varialbe inverse_matix. 
+## 1. MakeCacheMatrix() function : 
+##    The function makeCacheMatrix() will take a inversible matrix, first assign it as NULL. 
+##    Then Caculate the inverse of the matrix and assign it to the global varialbe inverse_matrix
+## 2. cacheSolve() function will read the global veriable inverse_matrix, if it is null, call the makeCacheMatix to assign it. 
+##    if the inverse_matrix is not NULL, give a message ( "The inverse_matrix is ready. No caculation! " )
+
+
+
+
+## initial the global varialbe 
 inverse_matrix <- c()
-makeCacheMatrix <- function(x = matrix()) {
-  inverse_matrix <<- NULL
-  print (inverse_matrix)
-  inverse_matrix <- solve(x)
-  print (inverse_matrix)
-}
+inverse_matrix1 <- c()
+inverse_matrix1 <- NULL
 
+## have two matrix ready for test
 A <- matrix( c(5, 1, 0,
                3,-1, 2,
                4, 0,-1), nrow=3, byrow=TRUE)
 
-B <- matrix ( c(), nrow=3, byrow=TRUE)
+B <- matrix ( c(3, 0, 2, 2, 0, -2, 0, 1, 1), nrow=3, byrow=TRUE)
 
-print (A)
-makeCacheMatrix (A)
+## frist funcition: read the matrix in, assign the global varialbe inverse_matrix as NULL. caculate the 
+## inverse_matrix  and assign it to the global variable 
 
-
-## initial the inverse_matrix as null. 
-## get the inverse_matrix as a global variable so that other function can use it
-## return the inverse_matrix
-
-cacheSolve <- function(x, ...) {
-  print (inverse_matrix)
-  ## Return a matrix that is the inverse of 'x'
+makeCacheMatrix<- function(x = matrix()){
+  
+  print (x)
+  inverse_matrix <<- NULL
+  R <- solve(x)
+  inverse_matrix <<- R
+  
+  return (inverse_matrix)
 }
 
-cacheSolve (A)
-## check if the inverse_matrix is availiable 
-## get it if it is in a varialbe
-## caculate it if it is not availialbe 
+
+## the function cacheSolve() read the global variable inverse_matrix and check if it is NULL. if is not NULLk, give a message 
+## "The inverse_matrix is ready. No caculation!. if it is NULL, then call makeCacheMatrix() to caculate the inverse_matrix 
+
+cacheSolve<- function(x = matrix()){
+    
+    if(is.null(inverse_matrix)){
+      message ("caculating... ")
+      new_inverse <- makeCacheMatrix(x)
+      print (new_inverse)
+      
+    }
+               
+    else message ("The inverse_matrix is ready. No caculation! ")
+  }
+  
+makeCacheMatrix(A)
+cacheSolve(B)
+ print (inverse_matrix)
+ 
+ 
